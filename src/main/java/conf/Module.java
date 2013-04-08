d659179670b448da608a321d04cf14762089c73d
rpo <rpo@atolcd.com>
2013-04-08 14:17:11 +0000

    modifying the application configuration

--- src/main/java/conf/Module.java
+++ src/main/java/conf/Module.java
@@ -1,19 +1,3 @@
-/**
- * Copyright (C) 2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package conf;
 
 import com.google.inject.AbstractModule;
@@ -21,23 +5,21 @@ import com.google.inject.AbstractModule;
 import etc.GreetingService;
 import etc.GreetingServiceImpl;
 
+/**
+ * Well, this is the Guice module.
+ *
+ * see also : http://google-guice.googlecode.com/svn/trunk/javadoc/com/google/inject/AbstractModule.html
+ */
 public class Module extends AbstractModule {
 
     public Module() {
-        super();     
+        super();
     }
 
-  
-    @Override
-    protected void configure() {       
-        // /////////////////////////////////////////////////////////////////////
-        // Some guice bindings
-        // /////////////////////////////////////////////////////////////////////
-        // some additional bindings for the application:
-        bind(GreetingService.class).to(GreetingServiceImpl.class);
-        // Bind the UDP ping controller so it starts up on server start
-        // bind(UdpPingController.class);
 
+    @Override
+    protected void configure() {
+        bind(GreetingService.class).to(GreetingServiceImpl.class);
     }
 
 }
